cf81185b6b1d3675b2fa459f79c0ed32eed05144
Abhijit Felix <abhijit1612s@gmail.com>
2025-04-30 19:27:26 +0530

    timeout increased

--- models/config/db.js
+++ models/config/db.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000,
+    });
     console.log("✅ MongoDB connected");
   } catch (err) {
     console.log("❌ MongoDB connection Error:", err);
