e67623a1d918a5cd9b74f98f76269f108a6638f1
Abhijit Felix <abhijit1612s@gmail.com>
2025-04-30 18:03:56 +0530

    views set added

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const post = require("./models/post");
 const connectToMongoDB = require("./models/config/db");
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
